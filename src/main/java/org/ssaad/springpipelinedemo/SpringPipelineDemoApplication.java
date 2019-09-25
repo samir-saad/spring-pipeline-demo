c32a608abb47fb5f97903f415e2d5ace14ade84a
Samir Saad <samir.saadallah@gmail.com>
2019-09-25 13:02:13 -0400

    Init Commit

---
+++ src/main/java/org/ssaad/springpipelinedemo/SpringPipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package org.ssaad.springpipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPipelineDemoApplication.class, args);
+	}
+
+}
